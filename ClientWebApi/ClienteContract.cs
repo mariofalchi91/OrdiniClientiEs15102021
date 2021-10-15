c345a01e02213cda800224578f36ad039c8cadde
Gian Mario Falchi <gian.mario.falchi@avanade.com>
2021-10-15 14:57:59 +0200

    client web api - get all clienti

---
+++ ClientWebApi/ClienteContract.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientWebApi
+{
+    internal class ClienteContract
+    {
+        public int Id { get; set; }
+        public string Codice { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+    }
+}
---
+++ ClientWebApi/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientWebApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("api client");
+
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage httpRequest = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://localhost:44376/api/ordini/clienti")
+            };
+
+            HttpResponseMessage httpResponse = await client.SendAsync(httpRequest);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                string jsonData = await httpResponse.Content.ReadAsStringAsync();
+                var results = JsonConvert.DeserializeObject<List<ClienteContract>>(jsonData);
+
+                foreach (var item in results)
+                    Console.WriteLine($"{item.Id} - {item.Nome} - {item.Cognome}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
